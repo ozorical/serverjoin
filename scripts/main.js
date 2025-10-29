fcb16b70a4abb841788bf31dbcbbf38615e66fea
ozorical <ozorical@icloud.com>
2025-10-29 22:38:52 +0000

    Server joiner script upload

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// Server Joining script
+// Allows you to transfer players from one server to another automatically.
+// useful for hub servers that send players to different game servers.
+// Made by Ozorical
+
+import {
+  world
+} from "@minecraft/server";
+import { transferPlayer } from "@minecraft/server-admin";
+world.afterEvents.playerSpawn.subscribe((event) => {
+transferPlayer(event.player, { hostname: "", port: 19132 }); // Enter server IP here, example: "play.crabsmp.net"
+});
+
